4d00657bb4480ae486a4e2d8b0bceffb49fdf964
satyam0027 <bharadwajsatyam033@gmail.com>
2024-11-18 17:41:59 +0530

    deploy error

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,19 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
+        protocol: "https",
+        hostname: "img.youtube.com",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      }
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
-}
+};
 
 export default nextConfig;
